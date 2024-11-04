67fc4657618cfefc6ba51e84d030491d1ae5e8ad
Hamza <hamzaoryakhil2@gmail.com>
2024-11-04 22:04:09 +0430

    Updates

---
+++ precache-manifest.34a48c2f433c110c7b45a36ad7277c9e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a76bf6a1b18255a7b48d25c6620662",
+    "url": "/bigbang/index.html"
+  },
+  {
+    "revision": "c85a1af92c1dd684a182",
+    "url": "/bigbang/static/css/main.66eec2be.chunk.css"
+  },
+  {
+    "revision": "5f3e0cd4e8e45eef87dd",
+    "url": "/bigbang/static/js/2.5d1d51ac.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/bigbang/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "c85a1af92c1dd684a182",
+    "url": "/bigbang/static/js/main.8d65743b.chunk.js"
+  },
+  {
+    "revision": "3da4a9d56ae5a531b2a0",
+    "url": "/bigbang/static/js/runtime~main.30663f0c.js"
+  },
+  {
+    "revision": "155bb000c10bf09289aaeb5dc9bfaf71",
+    "url": "/bigbang/static/media/earphone1.155bb000.jpg"
+  },
+  {
+    "revision": "0859d32395eb30654b6b23636017123b",
+    "url": "/bigbang/static/media/earphone2.0859d323.jpg"
+  },
+  {
+    "revision": "45a2cf0dc3de2b236b76ec79a6865f6a",
+    "url": "/bigbang/static/media/icons8-whatsapp-512.45a2cf0d.png"
+  },
+  {
+    "revision": "fe5210b848058c73b04ed1b310b35cdc",
+    "url": "/bigbang/static/media/powerbank.fe5210b8.jpg"
+  },
+  {
+    "revision": "217a560a834c88b308e71044e5d16cd4",
+    "url": "/bigbang/static/media/powerbank3.217a560a.webp"
+  },
+  {
+    "revision": "ba61385a5fc277057deb0f1fc0983ebd",
+    "url": "/bigbang/static/media/powerbank4.ba61385a.jpg"
+  },
+  {
+    "revision": "c115b7de3751572ab05b033575a61a9f",
+    "url": "/bigbang/static/media/smartwatch1.c115b7de.jpg"
+  },
+  {
+    "revision": "02d6201c4e108469e2e8aec6c002a2ee",
+    "url": "/bigbang/static/media/smartwatch2.02d6201c.jpeg"
+  }
+]);
